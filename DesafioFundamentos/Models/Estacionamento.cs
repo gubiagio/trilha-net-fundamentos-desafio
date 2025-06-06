fec36c289f6c9dbdb1bf730de6c5685e72dd81ba
gubiagio <gustavobiagio1998@gmail.com>
2025-06-06 14:09:21 -0300

    Backup!

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,7 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placaVeiculo = Console.ReadLine();
         }
 
         public void RemoverVeiculo()
